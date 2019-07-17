20197531983fecd2cd633964638783f28739789e
cgomesj <32676188+cgomesj@users.noreply.github.com>
2019-07-17 00:16:32 -0300

    vue-filters-mixins-v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Filters and Mixins</h1>
+        <p>{{ text }}</p>
+        <p>{{ text | toUppercase }}</p>
+        <p>{{ text | toUppercase | toLowercase }}</p>
+        <hr />
+        <input v-model="filterText" type="text" />
+        <ul class="list-unstyled">
+          <li
+            class="my-2"
+            v-for="(fruit, index) in filteredFruits"
+            :key="index"
+          >
+            {{ fruit }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
+
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+
+  data() {
+    return {
+      text: "Regular Text",
+      fruits: ["Banana", "Apple", "Mango", "Melon"],
+      filterText: ""
+    };
+  },
+
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter(element => {
+        return element.match(this.filterText);
+      });
+    }
   }
 };
 </script>
